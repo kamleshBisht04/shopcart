7e2da5f18098f24e74de23a9fef63bdfa284ecc5
kamleshBisht04 <kamleshbisht04@gmail.com>
2026-06-18 16:06:12 +0530

    project color preset define

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins`}>{children}</body>
+      <body className={`${poppins.variable} font-poppins antialiased `}>{children}</body>
     </html>
   );
 }
